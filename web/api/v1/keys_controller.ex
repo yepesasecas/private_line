d9a43b0162963f18e2c673054932744487717127
Andres Yepes <yepes07@gmail.com>
2016-11-07 18:17:10 -0500

    add public key endpoint

--- web/api/v1/keys_controller.ex
+++ web/api/v1/keys_controller.ex
@@ -5,7 +5,9 @@ defmodule PrivateLine.V1.KeysController do
     private_key_jwk = JOSE.JWK.from_pem_file("rsa-2048.pem")
     public_key_jwk = JOSE.JWK.to_public(private_key_jwk)
     {_, {_, public_key, _}} = public_key_jwk.kty
-    IO.inspect public_key_jwk
-    render conn, %{keys: %{public_key: public_key}}
+    IO.inspect public_key
+    encoded = Base.encode64("#{public_key}")
+    IO.inspect encoded
+    render conn, %{keys: %{public_key: encoded}}
   end
 end
