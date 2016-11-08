cf5fe730f0f44cc4f8e4822e4d2ea3853d502d7c
Andres Yepes <yepes07@gmail.com>
2016-11-08 14:02:07 -0500

    add a RSA module

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule PrivateLine.Mixfile do
   def application do
     [mod: {PrivateLine, []},
      applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex, :jose]]
+                    :phoenix_ecto, :postgrex]]
   end
 
   # Specifies which paths to compile per environment.
@@ -38,7 +38,7 @@ defmodule PrivateLine.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
-     {:jose, "~> 1.8"}]
+     {:rsa_ex, "~> 0.1"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
