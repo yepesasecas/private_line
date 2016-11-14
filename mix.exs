f95864c5b07dfad9b78622ab359b29d3a0d339ae
Andres Yepes <yepes07@gmail.com>
2016-11-14 16:45:17 -0500

    create release 0.0.1

--- mix.exs
+++ mix.exs
@@ -38,7 +38,9 @@ defmodule PrivateLine.Mixfile do
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
-     {:httpoison, "~> 0.10.0"}]
+    #  custom deps
+     {:httpoison, "~> 0.10.0"},
+     {:exrm, "~> 1.0"}]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
