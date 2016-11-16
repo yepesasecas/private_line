8f6e39b920974831bbf9dc11c9071215d13edc09
Andres Yepes <yepes07@gmail.com>
2016-11-16 18:55:55 -0500

    remove ecto supervisor

--- lib/private_line.ex
+++ lib/private_line.ex
@@ -9,7 +9,7 @@ defmodule PrivateLine do
     # Define workers and child supervisors to be supervised
     children = [
       # Start the Ecto repository
-      supervisor(PrivateLine.Repo, []),
+      # supervisor(PrivateLine.Repo, []),
       # Start the endpoint when the application starts
       supervisor(PrivateLine.Endpoint, []),
       # Start your own worker by calling: PrivateLine.Worker.start_link(arg1, arg2, arg3)
