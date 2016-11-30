9816b003e85874a66738e48c870b5b213c489d77
Andres Yepes <yepes07@gmail.com>
2016-11-30 16:16:07 -0300

    update views elixir best_practices

--- web/views/decrypt_and_merge.ex
+++ web/views/decrypt_and_merge.ex
@@ -1,15 +1,9 @@
 defmodule PrivateLine.V1.DecryptAndMergeView do
   use PrivateLine.Web, :view
 
-  def render("create.json", %{response: {:error, _stone, _destination_format, _destination_variables, msg}}) do
-    msg
-  end
-
+  def render("400.json", _assigns), do: %{error: "Bad Request"}
+  def render("create.json", %{response: {:error, _stone, _destination_format, _destination_variables, msg}}), do: %{error: msg}
   def render("create.json", %{response: {:ok, stone, destination_format, _destination_variables, _msg}}) do
     %{stone: stone, destination_format: destination_format}
-  end
-  
-  def render("400.json", _assigns) do
-    "Bad Request"
   end
 end
--- web/views/decrypt_view.ex
+++ web/views/decrypt_view.ex
@@ -1,15 +1,9 @@
 defmodule PrivateLine.V1.DecryptView do
   use PrivateLine.Web, :view
 
+  def render("400.json", _assigns), do: %{error: "Bad Request"}
+  def render("create.json", %{error_message: error_message}), do: %{error: error_message}
   def render("create.json", %{stone: stone}) do
-    stone
-  end
-
-  def render("create.json", %{error_message: error_message}) do
-    error_message
-  end
-
-  def render("400.json", _assigns) do
-    "Bad Request"
+    %{stone: stone}
   end
 end
