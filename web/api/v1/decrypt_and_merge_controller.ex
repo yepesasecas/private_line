7c478316cc586c64a47c4e62f1e1ac8236db45f7
Andres Yepes <yepes07@gmail.com>
2016-12-08 00:16:29 +0100

    fix decrypt_and_merge files names

---
+++ web/api/v1/decrypt_and_merge_controller.ex
@@ -0,0 +1,26 @@
+defmodule PrivateLine.V1.DecryptAndMergeController do
+  use PrivateLine.Web, :controller
+  alias PrivateLine.StoneDecrypt
+  alias PrivateLine.StoneMerger
+
+  def create(conn, %{"stone" => stone, "destination_format" => destination_format, "destination_variables" => destination_variables}) do
+    {status, stone, msg} = StoneDecrypt.decrypt(stone)
+    response = StoneMerger.merge({status, stone, destination_format, destination_variables, msg})
+
+    case response do
+      {:error, _stone, _destination_format, _destination_variables, _msg} ->
+        conn
+        |> put_status(400)
+        |> render("create.json", response: response)
+      _ ->
+        conn
+        |> render("create.json", response: response)
+    end
+  end
+
+  def create(conn, _params) do
+    conn
+    |> put_status(400)
+    |> render("400.json")
+  end
+end
